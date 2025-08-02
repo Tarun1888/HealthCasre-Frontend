14ee408d1e62ca10aa739405963147bb1832acc6
Tarun1888 <d757768@gmail.com>
2025-08-02 16:43:18 +0530

    Chnages

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -25,7 +25,7 @@ function Signup() {
     setSuccess('');
 
     try {
-      const res = await fetch('http://localhost:5000/api/signup', {
+      const res = await fetch('http://localhost:10000/api/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
